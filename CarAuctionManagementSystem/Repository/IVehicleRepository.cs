cc80606b2666298ab8b2198ffbea1e2cfb4486a1
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-02-06 14:38:26 +0200

    IVehicleRepository - edited

--- CarAuctionManagementSystem/Repository/IVehicleRepository.cs
+++ CarAuctionManagementSystem/Repository/IVehicleRepository.cs
@@ -1,6 +1,19 @@
+using CarAuctionManagementSystem.Models;
+
 namespace CarAuctionManagementSystem.Repository;
 
 public interface IVehicleRepository
 {
+    Vehicle?  GetById(int id);
+    
+    IEnumerable<Vehicle> GetAll();
+    IEnumerable<Vehicle> GetByType(VehicleType vehicleType);
+    IEnumerable<Vehicle> GetByManufacturer(string manufacturer);
+    
+    IEnumerable<Vehicle> GetByModel(string model);
+    IEnumerable<Vehicle> GetByYear(int year);
+    
+    void Insert(Vehicle vehicle);
+    void Update(Vehicle vehicle);
     
 }
