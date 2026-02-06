3d7cf1f00495eac269148d5be663218f634c4ce1
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-02-06 10:51:45 +0200

    OperationResult - Edit

--- CarAuctionManagementSystem/Utils/OperationResult.cs
+++ CarAuctionManagementSystem/Utils/OperationResult.cs
@@ -2,5 +2,8 @@ namespace CarAuctionManagementSystem.Utils;
 
 public class OperationResult
 {
-    
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public List<string> Errors { get; set; } = new();
+    public ErrorType ErrorType { get; set; } =  ErrorType.None;
 }
