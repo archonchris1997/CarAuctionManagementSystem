8937badf8b85fd8f912a07150022cd4a429db8c3
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-02-06 16:10:22 +0200

    IAuctionRepository - edited

--- CarAuctionManagementSystem/Repository/IAuctionRepository.cs
+++ CarAuctionManagementSystem/Repository/IAuctionRepository.cs
@@ -1,6 +1,15 @@
+using CarAuctionManagementSystem.Models;
+
 namespace CarAuctionManagementSystem.Repository;
 
 public interface IAuctionRepository
 {
+    IEnumerable<Auction> GetAll();
+    Auction? GetByVehicleId(int id);
+    Auction? GetById(int auctionId);
+    
+    void Insert(Auction auction);
+    void Update(Auction auction);
+    
     
 }
