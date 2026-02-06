e3fa298820949a0827bd650de92c24b4251e4c34
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-02-06 11:18:39 +0200

    Vehicle - Class edited

--- CarAuctionManagementSystem/Models/Vehicle.cs
+++ CarAuctionManagementSystem/Models/Vehicle.cs
@@ -2,5 +2,18 @@ namespace CarAuctionManagementSystem.Models;
 
 public class Vehicle
 {
+    public int Id { get; set; }
+    public VehicleType Type { get; set; }
+    
+    public string Model { get; set; }
+    public string Manufacturer { get; set; }
+    public int Year { get; set; }
+    public double StartingBid { get; set; }
+    
+    // Optional depending on Type
+    public int? NumberOfDoors { get; set; }   // Hatchback/Sedan
+    public int? NumberOfSeats { get; set; }   // Suv
+    public int? LoadCapacity { get; set; }    // Truck
+    
     
 }
