cbeebf6f5581f3d2bf12c1ced16737880492628a
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-02-07 23:16:04 +0200

    starting to implement the members of VehicleRepository

--- CarAuctionManagementSystem/Repository/VehicleRepository.cs
+++ CarAuctionManagementSystem/Repository/VehicleRepository.cs
@@ -1,6 +1,63 @@
+using CarAuctionManagementSystem.Models;
+
 namespace CarAuctionManagementSystem.Repository;
 
-public class VehicleRepository
+public class VehicleRepository : IVehicleRepository
 {
+    private readonly Dictionary<int, Vehicle> _vehicles = new();
+
+    
+    public Vehicle? GetById(int id)
+    {
+        if (_vehicles.TryGetValue(id, out var vehicleFound))
+        {
+            return vehicleFound;
+        }
+
+        return null;
+    }
+
+    public IEnumerable<Vehicle> GetAll()
+    {
+        return _vehicles.Values;
+    }
+
+    public IEnumerable<Vehicle> GetByType(VehicleType vehicleType)
+    {
+        return _vehicles.Values.Where(v => v.Type == vehicleType);
+    }
+
+    public IEnumerable<Vehicle> GetByManufacturer(string manufacturer)
+    {
+        return _vehicles.Values.Where(v => v.Manufacturer == manufacturer);
+    }
+
+    public IEnumerable<Vehicle> GetByModel(string model)
+    {
+        return _vehicles.Values.Where(v => v.Model == model);
+    }
+
+    public IEnumerable<Vehicle> GetByYear(int year)
+    {
+        return _vehicles.Values.Where(v => v.Year == year);
+    }
+
+    public void Insert(Vehicle vehicle)
+    {
+        _vehicles.Add(vehicle.Id, vehicle);
+
+    }
+
+    public void Update(Vehicle vehicle)
+    {
+
+        if (_vehicles.ContainsKey(vehicle.Id))
+        {
+            //todos os campos têm de ser *updated*
+            _vehicles[vehicle.Id] = vehicle;
+            
+        }
+    }
+    
     
 }
