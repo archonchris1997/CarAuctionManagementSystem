d5a1e8527c014981bef9cf12359fa7c9bcd75974
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-02-19 12:48:41 +0200

    .

--- CarAuctionManagementSystemTests/Services/VehicleServiceTests.cs
+++ CarAuctionManagementSystemTests/Services/VehicleServiceTests.cs
@@ -27,13 +27,49 @@ public class VehicleServiceTests
     [Fact]
     public void AddVehicle_WhenValidReturnsSuccess_AndInsertsOnce()
     {
-        //Arrange
+        //----------------------------- Arrange
         
+        // preparar o objeto que vou enviar para o Insert do Repositório
+        
+        var  vehicleRequest = new CreateVehicleRequest{
+            Type = VehicleType.Suv,
+            Id = 10,
+            Manufacturer = "Toyota",
+            Model = "RAV4",
+            Year = 2021,
+            StartingBid = 15000,
+            NumberOfSeats = 5
+        };
+
+        _mockCreateVehicleValidator.
+            Setup(v => v.Validate(vehicleRequest))
+            .Returns(new List<string>());
+        
+    
+        
+        //Forçar o sucesso nos ifs para isolar o serviço
+        _mockVehicleRepository
+            .Setup(r => r.GetById(10))
+            .Returns((Vehicle?)null);
+        //----------------------------------------------------------
         //Act
+        var result = _service.AddVehicle(vehicleRequest);
         
+        //----------------------------------------------------------
         //Assert
+
+        Assert.True(result.Success);
+        Assert.Equal(vehicleRequest.Id, result.Data.Id);
+        Assert.Equal("Vehicle created", result.Message);
         
+        //----------------------------------------------------------
         //Verify
+        
+        _mockVehicleRepository.Verify(repository => repository.Insert(It.Is<Vehicle>(v=>v.Id==10)), Times.Once);
+        _mockCreateVehicleValidator.Verify(validator=> validator.Validate(vehicleRequest), Times.Once);
+        
+        
+        
     }
     
     
